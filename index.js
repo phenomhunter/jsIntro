e593c8af906b8ea54793cda56ca333bcc44b638e
Orlando Mercado Tellado <67935991+phenomhunter@users.noreply.github.com>
2022-06-22 21:21:53 -0400

    Mario&Link%Coin gif, timeout function

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 let count = 0
 let saveEl = document.getElementById("save-el")
+let mario = document.getElementById("mario")
+let ans = true;
 function increment() {
-    count+=1
-    document.getElementById("count-el").innerText = count;
+    if(ans == true){ 
+        count+=1
+        document.getElementById("count-el").innerText = count;
+        mario.src = "coinGif.gif"
+        ans = false
+        setTimeout(tmo, 1000)
+    }
+
 }
 
 function save() {
     let entry = " " + count + " - "
     saveEl.innerText += entry
+    count = 0
+    document.getElementById("count-el").innerText = count
+    mario.src = "MarioGif.gif"
+}
+
+function tmo() {
+    ans = true
+    mario.src = "MarioGif.gif"
 }
